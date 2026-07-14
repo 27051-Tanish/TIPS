301e915f6defddf360e18c6f9619c3ab4f7c036e
Tanish <tanish.raffi@solitontech.com>
2026-07-14 10:05:56 +0530

    feat: make changes in console UI

--- src/Assignment1/program.cs
+++ src/Assignment1/program.cs
@@ -1,172 +1,215 @@
-﻿using System.ComponentModel;
-using System.Reflection.Metadata.Ecma335;
+﻿using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 
-var contacts = new List<String>();
-
-Console.WriteLine("Console-Based Contact Manager");
-Console.WriteLine();
-bool stopLoop = false;
-while (!stopLoop)
+namespace Assignments
 {
-    Console.WriteLine("===========================================");
-    Console.WriteLine("[1]. To Add New Contact: Press 'A' or 'a'");
-    Console.WriteLine("[2]. To View Contact: Press 'V' or 'v'");
-    Console.WriteLine("[3]. To Edit Contact: Press 'ED' or 'ed'");
-    Console.WriteLine("[4]. To Delete Contact: Press 'D' or 'd'");
-    Console.WriteLine("[5]. To Search Contact: Press 'S' or 's'");
-    Console.WriteLine("[6]. To Exit: Press 'E' or 'e'");
-    Console.WriteLine("===========================================");
-
-    var userChoice = Console.ReadLine();
-    switch (userChoice)
+    /// <summary>
+    /// First Assignment
+    /// </summary>
+    internal class Program
     {
-        case "A":
-        case "a":
-            Console.WriteLine("Add New Contact:");
-            Console.WriteLine();
-            Console.WriteLine("Enter name of the contact :");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Enter Phone Number :");
-            long phoneNumber = long.Parse(Console.ReadLine());
-
-            Console.WriteLine("Enter email address :");
-            string email = Console.ReadLine();
-            contacts.Add(name);
-            Console.WriteLine();
-            AddContactInfo(name, phoneNumber, email);
-            break;
-
-        case "V":
-        case "v":
-            ViewContactInfo();
-            break;
-
-        case "ED":
-        case "ed":
-            EditContactInfo();
-            break;
-
-        case "R":
-        case "r":
-            RemoveContactInfo();
-            break;
-
-        case "S":
-        case "s":
-            SearchContactInfo();
-            break;
-
-        case "E":
-        case "e":
-            stopLoop = true;
-            break;
-    }
-}
-
-void AddContactInfo(string name, long phoneNumber, string email)
-{
-    Console.WriteLine("Name :" + name);
-    Console.WriteLine("Phone Number :" + phoneNumber);
-    Console.WriteLine("Email :" + email);
-    Console.WriteLine();
-    Console.WriteLine("Contact added successfully");
-}
-void ViewContactInfo()
-{
-        if(contacts.Count == 0)
+        /// <summary>
+        /// main method
+        /// </summary>
+        /// <param name="args">Console-Based Contact Manager</param>
+        public static void Main(string[] args)
         {
-        Console.WriteLine("There is no contact present in the Contact Manager");
-        }
-        else
-        {
-        for (int i = 0; i < contacts.Count; i++)
+            List<List<string>> contacts = new List<List<string>>();
+
+            Console.WriteLine("Console-Based Contact Manager");
+            Console.WriteLine();
+            bool stopLoop = false;
+            while (!stopLoop)
             {
-            Console.WriteLine($"{i+1}. {contacts[i]}");
+                Console.WriteLine("===========================================");
+                Console.WriteLine("[1]. To Add New Contact: Press 'A' or 'a'");
+                Console.WriteLine("[2]. To View Contact: Press 'V' or 'v'");
+                Console.WriteLine("[3]. To Edit Contact: Press 'ED' or 'ed'");
+                Console.WriteLine("[4]. To Delete Contact: Press 'D' or 'd'");
+                Console.WriteLine("[5]. To Search Contact: Press 'S' or 's'");
+                Console.WriteLine("[6]. To Exit: Press 'E' or 'e'");
+                Console.WriteLine("===========================================");
+
+                var userChoice = Console.ReadLine();
+                switch (userChoice)
+                {
+                    case "A":
+                    case "a":
+                        AddContactInfo();
+                        break;
+
+                    case "V":
+                    case "v":
+                        ViewContactInfo();
+                        break;
+
+                    case "ED":
+                    case "ed":
+                        EditContactInfo();
+                        break;
+
+                    case "D":
+                    case "d":
+                        RemoveContactInfo();
+                        break;
+
+                    case "S":
+                    case "s":
+                        SearchContactInfo();
+                        break;
+
+                    case "E":
+                    case "e":
+                        stopLoop = true;
+                        break;
+                }
             }
-        }
-}
-void EditContactInfo()
-{
-    bool isEditable = false;
-    while (!isEditable)
-    {
-        if (contacts.Count == 0)
-        {
-            Console.WriteLine("There is no contact in the manager");
-            continue;
-        }
-        Console.WriteLine("Enter the index of the contact to edit: ");
-        var userInput = Console.ReadLine();
-        if (int.TryParse(userInput, out int index) && index >= 1 && index <= contacts.Count)
-        {
-            var contactToBeEdited = contacts[index - 1];
-            Console.WriteLine("Enter New Name: ");
-            string newName = Console.ReadLine();
-            contactToBeEdited = newName;
-            Console.WriteLine("Edited the contact info successfully");
-            ViewContactInfo();
-            isEditable = true;
-        }
-        else
-        {
-            Console.WriteLine("There is no such contact");
-        }
-    }
-}
-void RemoveContactInfo()
-{
-    if (contacts.Count == 0)
-    {
-        Console.WriteLine("No Contacts have been added yet");
-        return;
-    }
-    bool isIndexValid = false;
+            void AddContactInfo()
+            {
+                List<string> contact = new List<string>(); 
+                Console.WriteLine("Add New Contact:");
+                Console.WriteLine();
+                Console.WriteLine("Enter name of the contact :");
+                contact.Add(Console.ReadLine());
 
-    while (!isIndexValid)
-    {
-        Console.WriteLine("Enter the index of the contact to delete: ");
-        ViewContactInfo();
-        var removeIndex = Console.ReadLine();
-        if (removeIndex == "")
-        {
-            Console.WriteLine("Remove Index should not be empty");
-            continue;
-        }
-        if (int.TryParse(removeIndex, out int index) && index >= 1 && index <= contacts.Count)
-        {
-            var contactToBeRemoved = contacts[index - 1];
-            contacts.RemoveAt(index - 1);
-            Console.WriteLine("Contact" + contactToBeRemoved + " deleted successfully");
-            isIndexValid = true;
-            ViewContactInfo();
-        }
-        else
-        {
-            Console.WriteLine("The given index is not valid");
+                Console.WriteLine("Enter Phone Number :");
+                contact.Add(Console.ReadLine());
+
+                Console.WriteLine("Enter email address :");
+                contact.Add(Console.ReadLine());
+
+                Console.WriteLine("Name :" + contact[0]);
+                Console.WriteLine("Phone Number :" + contact[1]);
+                Console.WriteLine("Email :" + contact[2]);
+                Console.WriteLine();
+                contacts.Add(contact);
+                Console.WriteLine("Contact added successfully");
+            }
+            void ViewContactInfo()
+            {
+                if (contacts.Count == 0)
+                {
+                    Console.WriteLine("There is no contact present in the Contact Manager");
+                }
+                else
+                {
+                    contacts.Sort((a, b) => a[0].CompareTo(b[0]));
+                    for (int i = 0; i < contacts.Count; i++)
+                    {
+                        Console.WriteLine($"{i + 1}.Name: {contacts[i][0]}");
+                        Console.WriteLine($"{i + 1}.Phone: {contacts[i][1]}");
+                        Console.WriteLine($"{i + 1}.Email: {contacts[i][2]}");
+                        Console.WriteLine("--------------------------------");
+                    }
+                }
+            }
+            void EditContactInfo()
+            {
+                bool isEditable = false;
+                while (!isEditable)
+                {
+                    if (contacts.Count <= 0)
+                    {
+                        Console.WriteLine("There is no contact in the manager");
+                        continue;
+                    }
+                    Console.WriteLine("Enter the name of the contact to edit: ");
+                    var userInput = Console.ReadLine();
+                    foreach(List<string> c in contacts)
+                    {
+                        if (userInput.ToLower() == c[0].ToLower())
+                        {
+                            Console.WriteLine("Which contact info you need to edit: ");
+                            Console.WriteLine();
+                            Console.WriteLine("Name, Phone, Email");
+                            var userOption = Console.ReadLine();
+                            if (userOption == "Name")
+                            {
+                                Console.WriteLine("Enter new Name: ");
+                                var newName = Console.ReadLine();
+                                c[0] = newName;
+                            }
+                            else if (userOption == "Phone")
+                            {
+                                Console.WriteLine("Enter new number: ");
+                                var newNumber = Console.ReadLine();
+                                c[1] = newNumber;
+                            }
+                            else if(userOption == "Email")
+                            {
+                                Console.WriteLine("Enter new email: ");
+                                var newEmail = Console.ReadLine();
+                                c[2] = newEmail;
+                            }
+                            isEditable = true;
+                            return;
+                        }
+                    } 
+                }
+                Console.WriteLine("There is no such contact");
+            }
+            void RemoveContactInfo()
+            {
+                if (contacts.Count <= 0)
+                {
+                    Console.WriteLine("No Contacts have been added yet");
+                    return;
+                }
+                bool isIndexValid = false;
+
+                while (!isIndexValid)
+                {
+                    Console.WriteLine("Enter the name of the contact to delete: ");
+                    ViewContactInfo();
+                    var removeName = Console.ReadLine();
+                    if (removeName == "")
+                    {
+                        Console.WriteLine("Name field should not be empty");
+                        continue;
+                    }
+                    for (int i = 0; i < contacts.Count(); i++)
+                    {
+                        if (removeName.ToLower() == contacts[i][0].ToLower())
+                        {
+                            contacts.RemoveAt(i);
+                            Console.WriteLine("Contact deleted successfully");
+                            isIndexValid = true;
+                            ViewContactInfo();
+                            return;
+                        }
+                    }
+                    Console.WriteLine("There is no such contact");
+                }
+            }
+            void SearchContactInfo()
+            {
+                bool isNameValid = false;
+                while (!isNameValid)
+                {
+                    Console.WriteLine("Enter name to search: ");
+                    var searchName = Console.ReadLine();
+                    if (searchName.Length == 0)
+                    {
+                        Console.WriteLine("Not a valid input");
+                        continue;
+                    }
+
+                    foreach (List<string> c in contacts)
+                    {
+                        if (searchName.ToLower() == c[0].ToLower())
+                        {
+                            isNameValid = true;
+                            Console.WriteLine("Contact Found :" + c[0]);
+                        }
+                        else
+                        {
+                            Console.WriteLine("There is no such contact");
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
-void SearchContactInfo()
-{
-    bool isNameValid = false;
-    Console.WriteLine("Enter name to search: ");
-    var searchName = Console.ReadLine();
-    if (searchName.Length == 0)
-    {
-        Console.WriteLine("Not a valid input");
-    }
-
-    string foundName = contacts.Find(s => s.Equals(searchName, StringComparison.OrdinalIgnoreCase));
-    if (foundName != null)
-    {
-        isNameValid = true;
-        Console.WriteLine("Contact Found :" + foundName);
-    }
-    else
-    {
-        Console.WriteLine("There is no such contact");
-    }
-}
-
-Console.ReadKey();
