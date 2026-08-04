238938948f09efa6fde1bdb8047924871e940553
27051-Tanish <tanish.raffi@solitontech.com>
2026-08-04 09:24:03 +0530

    feat : add expense tracker project

---
+++ src/ExpenseTracker/Model/TrackerInfo.cs
@@ -0,0 +1,26 @@
+﻿namespace ExpenseTracker.Model
+{
+    /// <summary>
+    /// Provides a base contract and shared properties for expense tracker objects.
+    /// </summary>
+    internal class TrackerInfo
+    {
+        /// <summary>
+        /// Gets or sets id.
+        /// </summary>
+        /// <value>
+        /// Id of the particular tracker detail.
+        /// </value>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the amount.
+        /// </summary>
+        /// <value>
+        /// The amount of the 
+        /// </value>
+        public decimal Amount { get; set; }
+
+        public string Category { get; set; }
+
+        public DateTime Date { get; set; }
