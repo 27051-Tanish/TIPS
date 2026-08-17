17d2e2e3cefcb344d9ed2f92709ef00e9eddc480
27051-Tanish <tanish.raffi@solitontech.com>
2026-08-17 09:30:44 +0530

    Feature: Inventory Management (#3)
    * feat: create mvc folder structure for inventory management
    
    * feat: introduce add, delete and view feature
    
    * feat: build application without handling exception
    
    * fix: handle inputs using conditional statements
    
    * feat: handle custom exception for duplicate id
    
    * fix : resolve peer review comments
    
    * feat : add console tables for better view
    
    * fix: resolve review comments and introduce interface
    
    * fix : resolve build solution test error
    
    * feat : implement interface
    
    * fix : change return statement in vaidation
    
    * fix: resolve review comments
    
    * feat: limit the input to 5 retries
    
    * refactor: reuse search functionality

---
+++ src/InventoryManagement/Repository/InMemoryStorage.cs
@@ -0,0 +1,54 @@
+﻿using InventoryManagement.Helper;
+using InventoryManagement.Model;
+
+namespace InventoryManagement.Repository
+{
+    /// <summary>
+    /// In-memory repository stores the list of products.
+    /// </summary>
+    public class InMemoryStorage : IInventoryRepository
+    {
+        private readonly List<InventoryInfo> _inventories = new ();
+
+        /// <inheritdoc/>
+        public void AddItems(InventoryInfo item)
+        {
+            this._inventories.Add(item);
+        }
+
+        /// <inheritdoc/>
+        public void RemoveItems(InventoryInfo item)
+        {
+            if (!InputValidator.ValidateId(item.Id))
+            {
+                throw new InvalidOperationException("Please enter valid ID.");
+            }
+
+            this._inventories.Remove(item);
+        }
+
+        /// <inheritdoc/>
+        public List<InventoryInfo> GetAllItems()
+        {
+            return this._inventories;
+        }
+
+        /// <inheritdoc/>
+        public void UpdateItems(InventoryInfo item)
+        {
+            InventoryInfo? oldItem = this.GetItemById(item.Id);
+            if (oldItem != null)
+            {
+                oldItem.Name = item.Name;
+                oldItem.Price = item.Price;
+                oldItem.Quantity = item.Quantity;
+            }
+        }
+
+        /// <inheritdoc/>
+        public InventoryInfo? GetItemById(string? id)
+        {
+            return this._inventories.Find(item => item.Id == id);
+        }
+    }
+}
