2d4818ee761c8c32c204d941cbebb1862aea036a
27051-Tanish <tanish.raffi@solitontech.com>
2026-09-04 19:25:42 +0530

    Feature | Error Handling | Tanish (#7)
    * feat: add error handling tasks
    
    * refactor: remove the unwanted exception thrown
    
    * feat: add screenshot of stack trace
    
    * chore: update local
    
    * chore: update local
    
    * chore: resolve build error
    
    * fix: update the assignment number in the md file

---
+++ src/ErrorHandling/Exceptions/InvalidUserInputException.cs
@@ -0,0 +1,17 @@
+﻿namespace ErrorHandling.Exceptions
+{
+    /// <summary>
+    /// If the user enters an invalid input, throw an InvalidUserInputException.
+    /// </summary>
+    public class InvalidUserInputException : Exception
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="InvalidUserInputException"/> class.
+        /// </summary>
+        /// <param name="message">The message to be displayed.</param>
+        public InvalidUserInputException(string message)
+            : base(message)
+        {
+        }
+    }
+}
