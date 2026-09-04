7d9abe56200fd428eff491a10a3b04731320a817
27051-Tanish <tanish.raffi@solitontech.com>
2026-09-04 16:57:27 +0530

    feat: finish writer method

---
+++ src/MemoryManagemenProject/IDisposableDemo/FileOperations/FileReader.cs
@@ -0,0 +1,33 @@
+﻿namespace IDisposableDemo.FileOperations
+{
+    /// <summary>
+    /// Reads the content from the file.
+    /// </summary>
+    public class FileReader : IDisposable
+    {
+        private readonly StreamReader _reader;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FileReader"/> class.
+        /// </summary>
+        /// <param name="filePath">The path of the file.</param>
+        public FileReader(string filePath)
+        {
+            this._reader = new StreamReader(filePath);
+        }
+
+        /// <summary>
+        /// Read from the file.
+        /// </summary>
+        public void ReadFromFile()
+        {
+            this._reader.ReadToEnd();
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this._reader.Dispose();
+        }
+    }
+}
