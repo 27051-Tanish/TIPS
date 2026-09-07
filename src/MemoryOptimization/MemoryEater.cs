bcdd80c1194b93bbe1656d42b4bc981bc54bfe3e
27051-Tanish <tanish.raffi@solitontech.com>
2026-09-07 12:30:31 +0530

    feat: identify memory issue in the code snippet and fix them

---
+++ src/MemoryOptimization/MemoryEater.cs
@@ -0,0 +1,25 @@
+﻿namespace MemoryOptimization
+{
+    /// <summary>
+    /// Fixes a memory issue provided in the code snippet and implement memory management best practices.
+    /// </summary>
+    public class MemoryEater
+    {
+        /// <summary>
+        /// List of integer array to demonstrate memory management best practices.
+        /// </summary>
+        private List<int[]> _memAlloc = new List<int[]>();
+
+        /// <summary>
+        /// Adds new integer array to the list.
+        /// </summary>
+        public void Allocate()
+        {
+            while (true)
+            {
+                this._memAlloc.Add(new int[1000]);
+                Thread.Sleep(10);
+            }
+        }
+    }
+}
---
+++ src/MemoryOptimization/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace MemoryOptimization
+{
+    /// <summary>
+    /// Contains the execution logic of the project.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Provides the entry point for the main execution of the project.
+        /// </summary>
+        public static void Main()
+        {
+            MemoryEater me = new MemoryEater();
+            me.Allocate();
+            Console.ReadKey();
+        }
+    }
+}
