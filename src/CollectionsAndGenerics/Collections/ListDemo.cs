40c8d91fd5497602bd80c4e7474e4723956b0dcf
27051-Tanish <tanish.raffi@solitontech.com>
2026-09-09 09:27:08 +0530

    feat: create project for list task

---
+++ src/CollectionsAndGenerics/Collections/ListDemo.cs
@@ -0,0 +1,11 @@
+﻿namespace CollectionsAndGenerics.Collections
+{
+    /// <summary>
+    /// Performs different operations in a list.
+    /// </summary>
+    public class ListDemo
+    {
+        private readonly List<string> _list = new List<string>();
+
+    }
+}
