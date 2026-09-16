9eae92c34c92450b21468ecd24bbee82a9e5baf7
27051-Tanish <tanish.raffi@solitontech.com>
2026-09-16 18:45:58 +0530

    feat: make initial setup

---
+++ src/AdvancedCSharpConcepts/Enum/MainMenu.cs
@@ -0,0 +1,48 @@
+﻿namespace AdvancedCSharpConcepts.Enum
+{
+    /// <summary>
+    /// Represents various options of the main menu.
+    /// </summary>
+    public enum MainMenu
+    {
+        /// <summary>
+        /// Represents the implementation of events and delegates.
+        /// </summary>
+        Task1 = 1,
+
+        /// <summary>
+        /// Represents the implementation of dynamic and var keywords.
+        /// </summary>
+        Task2,
+
+        /// <summary>
+        /// Represents the implementation of anonymous methods.
+        /// </summary>
+        Task3,
+
+        /// <summary>
+        /// Represents the implementation of lambda expressions and statements.
+        /// </summary>
+        Task4,
+
+        /// <summary>
+        /// Represents the implementation of advanced use of delegates for sorting.
+        /// </summary>
+        Task5,
+
+        /// <summary>
+        /// Represents the implementation of records.
+        /// </summary>
+        Task6,
+
+        /// <summary>
+        /// Represents the implementation of advanced pattern matching.
+        /// </summary>
+        Task7,
+
+        /// <summary>
+        /// Represents the exit option.
+        /// </summary>
+        Exit,
+    }
+}
---
+++ src/AdvancedCSharpConcepts/View/ConsoleView.cs
@@ -0,0 +1,18 @@
+﻿namespace AdvancedCSharpConcepts.View
+{
+    /// <summary>
+    /// Provides console-based UI methods for displaying messages,
+    /// and reading user input for various application modules.
+    /// </summary>
+    public class ConsoleView
+    {
+        /// <summary>
+        /// Writes the content to the UI.
+        /// </summary>
+        /// <param name="message">The message to be written.</param>
+        public void ShowMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
